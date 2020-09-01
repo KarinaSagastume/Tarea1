d995cf0231bb03c927b7c973f33b3470c9513349
unknown <kary_sagastume@live.com>
2020-08-31 20:43:13 -0500

    Primera tarea

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="App">
+      <title className="App-header">
+        <h1 className="App-title"> Condiciones Meteorológicas</h1>
+      
+      </title>
+    </div>
+  );
+}
+
+
+
+}
+
+
+ 
+export default App;
